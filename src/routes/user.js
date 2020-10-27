8d11a041fc71591b1b7c237a37228cc29cb51f4a
Heinrick Senna <marcelohz198@hotmail.com>
2020-10-27 17:45:30 -0300

    Primeiro Usuário Criado

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,7 @@ const userController = require('../controllers/user');
 
 module.exports = (app) => {
 
-	app.route('/v1/users')
+	app.route('/complacencyclass.com.br/v1/users')
 		.get(userController.allUsers)
 		.post(userController.newUser);
 }
